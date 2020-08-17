201bdaa01a7a2b315da88ba7f312605bb33119ef
ArmandoTapia <rodney.tapia@epn.edu.ec>
2020-08-17 11:52:51 -0500

    Add some changes

--- src/app/shared/var.constant.ts
+++ src/app/shared/var.constant.ts
@@ -1,2 +1,2 @@
-export const HOST = 'http://localhost:5000';
-// export const HOST = 'https://backendtodolegal.herokuapp.com';
+// export const HOST = 'http://localhost:5000';
+export const HOST = 'https://backendtodolegal.herokuapp.com';
